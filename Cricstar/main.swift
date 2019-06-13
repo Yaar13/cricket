5358b0e2fd9c70c6fdc087dc1bc46b187281d990
parag <paraggarg66@yahoo.in>
2019-06-12 21:12:48 -0400

    Initial Commit

---
+++ Cricstar/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Cricstar
+//
+//  Created by Gurparkash Kaur on 2019-06-12.
+//  Copyright © 2019 Parag Garg. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
